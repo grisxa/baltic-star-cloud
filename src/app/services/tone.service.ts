c674896c58d322e03641a974a300fcf7a2f7abf6
Grigorii Batalov <grigorii.batalov@clarivate.com>
2020-07-04 13:03:26 +0300

    Tune volume up with GainNode

--- src/app/services/tone.service.ts
+++ src/app/services/tone.service.ts
@@ -5,11 +5,16 @@ import {Injectable} from '@angular/core';
 })
 export class ToneService {
   audioContext: AudioContext;
+  gainNode: GainNode;
 
   constructor() {
     // @ts-ignore
     const AudioContext = window.AudioContext || window.webkitAudioContext;
     this.audioContext = new AudioContext();
+    this.gainNode = this.audioContext.createGain();
+    this.gainNode.connect(this.audioContext.destination);
+    // set the maximum volume
+    this.gainNode.gain.value = this.gainNode.gain.maxValue;
   }
 
   /**
@@ -22,10 +27,10 @@ export class ToneService {
     oscillator.frequency.value = frequency;
     oscillator.type = 'sawtooth';
     oscillator.start();
-    oscillator.connect(this.audioContext.destination);
+    oscillator.connect(this.gainNode);
 
     setTimeout(() => {
-      oscillator.disconnect(this.audioContext.destination);
+      oscillator.disconnect(this.gainNode);
       oscillator.stop();
     }, length);
   }
