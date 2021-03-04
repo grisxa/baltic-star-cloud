acdb87dbadf7d979869630e274839587cb75f340
Grigorii Batalov <grisxa@gmail.com>
2021-03-04 19:12:55 +0300

    Testing collapsing of BrevetArchiveComponent

--- src/app/components/brevet-archive/brevet-archive.component.spec.ts
+++ src/app/components/brevet-archive/brevet-archive.component.spec.ts
@@ -1,6 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatListModule} from '@angular/material/list';
+import {By} from '@angular/platform-browser';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 import {BrevetArchiveComponent} from './brevet-archive.component';
@@ -30,4 +31,19 @@ describe('BrevetArchiveComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a closed list by default', () => {
+    const panel = fixture.debugElement
+      .query(By.css('.mat-expansion-panel-header-description'));
+    expect(panel.nativeElement.innerText).toEqual('Expand');
+  });
+
+  it('should allow collapsing after opening', () => {
+    component.panelOpenState = true;
+    fixture.detectChanges();
+    const panel = fixture.debugElement
+      .query(By.css('.mat-expansion-panel-header-description'));
+    expect(panel.nativeElement.innerText).toEqual('Collapse');
+  });
+
 });
