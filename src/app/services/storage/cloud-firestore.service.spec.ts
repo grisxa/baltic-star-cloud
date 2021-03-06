6d804ddf0787555972f0a26b2f947468847112af
Grigorii Batalov <grisxa@gmail.com>
2021-03-06 07:56:35 +0300

    Simple Cloud Firestore test enabled

--- src/app/services/storage/cloud-firestore.service.spec.ts
+++ src/app/services/storage/cloud-firestore.service.spec.ts
@@ -1,12 +1,18 @@
 import {TestBed} from '@angular/core/testing';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../../environments/environment';
 
 import {CloudFirestoreService} from './cloud-firestore.service';
 
-xdescribe('CloudFirestoreService', () => {
+describe('CloudFirestoreService', () => {
   let service: CloudFirestoreService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+      ],
+    });
     service = TestBed.inject(CloudFirestoreService);
   });
 
