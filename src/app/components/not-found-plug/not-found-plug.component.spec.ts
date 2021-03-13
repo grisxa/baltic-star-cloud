a98096df0fd11a45f56438aa1f0a87e190c1c8db
Grigorii Batalov <grisxa@gmail.com>
2021-03-13 11:35:26 +0300

    Not Found test updated

--- src/app/components/not-found-plug/not-found-plug.component.spec.ts
+++ src/app/components/not-found-plug/not-found-plug.component.spec.ts
@@ -2,15 +2,14 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {NotFoundPlugComponent} from './not-found-plug.component';
 
-fdescribe('NotFoundPlugComponent', () => {
+describe('NotFoundPlugComponent', () => {
   let component: NotFoundPlugComponent;
   let fixture: ComponentFixture<NotFoundPlugComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NotFoundPlugComponent ]
-    })
-    .compileComponents();
+      declarations: [NotFoundPlugComponent]
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +20,9 @@ fdescribe('NotFoundPlugComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should say Not found', () => {
+    expect(fixture.nativeElement.innerText).toEqual('Not found');
   });
 });
