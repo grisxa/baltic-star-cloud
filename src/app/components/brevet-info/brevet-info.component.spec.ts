46a8eebde6341af115eda4ced111d94bfff3d415
Grigorii Batalov <grisxa@gmail.com>
2021-03-13 11:43:15 +0300

    BrevetInfo test updated

--- src/app/components/brevet-info/brevet-info.component.spec.ts
+++ src/app/components/brevet-info/brevet-info.component.spec.ts
@@ -1,6 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MatListModule} from '@angular/material/list';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Observable, of} from 'rxjs';
+import {Brevet, BrevetOptions} from '../../models/brevet';
+import {CloudFirestoreService} from '../../services/storage/cloud-firestore.service';
+import {LoadingPlugComponent} from '../loading-plug/loading-plug.component';
 
-import { BrevetInfoComponent } from './brevet-info.component';
+import {BrevetInfoComponent} from './brevet-info.component';
+
+class MockStorageService {
+  getBrevet(uid: string): Observable<Brevet> {
+    return of(new Brevet('Горьковский', {
+      uid: '4',
+      length: 200,
+      startDate: new Date('2020-09-26T06:00:00')
+    } as BrevetOptions));
+  }
+}
 
 describe('BrevetInfoComponent', () => {
   let component: BrevetInfoComponent;
@@ -8,9 +24,15 @@ describe('BrevetInfoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BrevetInfoComponent ]
-    })
-    .compileComponents();
+      declarations: [BrevetInfoComponent,LoadingPlugComponent],
+      imports: [
+        MatListModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        {provide: CloudFirestoreService, useClass: MockStorageService},
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +43,9 @@ describe('BrevetInfoComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load a mocked Brevet object', () => {
+    expect(component.brevet.uid).toEqual('4');
   });
 });
