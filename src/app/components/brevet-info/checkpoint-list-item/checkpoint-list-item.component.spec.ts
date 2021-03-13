34519049c960d446e8ed8222932c69f0090a731f
Grigorii Batalov <grisxa@gmail.com>
2021-03-13 11:37:07 +0300

    CheckpointListItem test updated

--- src/app/components/brevet-info/checkpoint-list-item/checkpoint-list-item.component.spec.ts
+++ src/app/components/brevet-info/checkpoint-list-item/checkpoint-list-item.component.spec.ts
@@ -1,6 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MatListModule} from '@angular/material/list';
+import {By} from '@angular/platform-browser';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Checkpoint} from '../../../models/checkpoint';
+import {Waypoint} from '../../../models/waypoint';
 
-import { CheckpointListItemComponent } from './checkpoint-list-item.component';
+import {CheckpointListItemComponent} from './checkpoint-list-item.component';
 
 describe('CheckpointListItemComponent', () => {
   let component: CheckpointListItemComponent;
@@ -8,9 +13,12 @@ describe('CheckpointListItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CheckpointListItemComponent ]
-    })
-    .compileComponents();
+      declarations: [CheckpointListItemComponent],
+      imports: [
+        MatListModule,
+        RouterTestingModule,
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +29,13 @@ describe('CheckpointListItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render a checkpoint record', () => {
+    component.checkpoint = new Checkpoint({name: 'test', distance: 1} as Waypoint);
+    fixture.detectChanges();
+    const link = fixture.debugElement
+      .query(By.css('mat-list-item'));
+    expect(link.nativeElement.innerText).toEqual('test, 1 km');
   });
 });
