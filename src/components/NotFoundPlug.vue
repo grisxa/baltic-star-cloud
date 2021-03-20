9fa05ae2e5a0857ac61ce9a35c642030ecc2c4ca
Grigorii Batalov <grisxa@gmail.com>
2021-03-20 05:33:36 +0300

    A plug for not found documents

---
+++ src/components/NotFoundPlug.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ $t('NotFoundPlug.title') }}</div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component
+export default class NotFoundPlug extends Vue {
+}
+
+Vue.component('app-not-found-plug', NotFoundPlug);
+</script>
+
+<style scoped lang="scss">
+div {
+  padding: 1rem;
+}
+</style>
