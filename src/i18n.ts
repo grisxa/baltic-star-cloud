87baffd5f995e0510359123757ebeaf2df2ba03c
Grigorii Batalov <grisxa@gmail.com>
2021-03-20 05:27:11 +0300

    A language (locale) switcher

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,7 @@
+import ElementLocale from 'element-ui/lib/locale';
+import enLocale from 'element-ui/lib/locale/lang/en';
 import Vue from 'vue';
-import VueI18n, { LocaleMessages } from 'vue-i18n';
+import VueI18n, {LocaleMessages} from 'vue-i18n';
 
 Vue.use(VueI18n);
 
@@ -13,11 +15,17 @@ function loadLocaleMessages(): LocaleMessages {
       messages[locale] = locales(key).default;
     }
   });
+  messages.en = {...messages.en, ...enLocale};
   return messages;
 }
 
-export default new VueI18n({
+const i18n = new VueI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
   messages: loadLocaleMessages(),
 });
+
+ElementLocale.i18n((key: string,
+  value: string[] | { [key: string]: string; } | undefined) => i18n.t(key, value));
+
+export default i18n;
