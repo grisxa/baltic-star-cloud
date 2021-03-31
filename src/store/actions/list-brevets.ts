611e7538e588174f4b20da806200483386b88678
Grigorii Batalov <grisxa@gmail.com>
2021-03-31 06:16:32 +0300

    Switch to AWS REST API

--- src/store/actions/list-brevets.ts
+++ src/store/actions/list-brevets.ts
@@ -1,18 +1,13 @@
 import Brevet from '@/models/brevet';
 import Checkpoint from '@/models/checkpoint';
 import {State} from '@/models/state';
-import {loadMap} from '@/store/models/firestore';
 import SetBrevetMutation from '@/store/models/setBrevetMutation';
 import SetLoadingMutation from '@/store/models/setLoadingMutation';
 import axios from 'axios';
 import {ActionContext} from 'vuex';
 
 export default function (context: ActionContext<State, State>): void {
-  const suffix = '.beta';
-  const base = 'https://firestore.googleapis.com/v1';
-  const projectId = 'baltic-star-cloud';
-  const path = `brevets${suffix}/list`;
-  const url = `${base}/projects/${projectId}/databases/(default)/documents/${path}`;
+  const url = `${process.env.VUE_APP_AWS_API}/brevets`;
 
   if (!navigator.onLine) {
     console.error('Network error');
@@ -23,24 +18,25 @@ export default function (context: ActionContext<State, State>): void {
   axios
     .get(url, {timeout: 60000})
     .then((response) => {
-      // console.log('raw reply', response.data);
-      const data = loadMap(response.data);
-      // console.log(' = got brevets', data);
-      if (data.brevets instanceof Array) {
-        const brevets = data.brevets
-          .map((item) => {
-            const {checkpoints} = item as any;
-            const objCP = checkpoints instanceof Array ? checkpoints.filter((cp) => !!cp)
-              .map((cp) => new Checkpoint(cp)) : checkpoints;
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            return new Brevet({...item, checkpoints: objCP});
+      const brevets = response.data
+        .map((item: Brevet) => {
+          const {checkpoints} = item;
+          const objCP = checkpoints instanceof Array ? checkpoints.filter((cp) => !!cp)
+            .map((cp) => new Checkpoint(cp)) : checkpoints;
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          return new Brevet({
+            ...item,
+            startDate: new Date(item.startDate),
+            endDate: (item.endDate && new Date(item.endDate)),
+            checkpoints: objCP,
           });
-        context.commit(new SetBrevetMutation(brevets));
-      }
+        })
+        .sort((a: Brevet, b: Brevet) => a.startDate.valueOf() - b.startDate.valueOf());
+      context.commit(new SetBrevetMutation(brevets));
     })
     .catch((error) => {
-      // TODO: set a steate error and show
+      // TODO: set a state error and show
       console.error('get doc', error.message, error);
     })
     .finally(() => context.commit(new SetLoadingMutation(false)));
