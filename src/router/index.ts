e52e56e5bfb9e27b61cdd5205480ce31d61aad25
Grigorii Batalov <grisxa@gmail.com>
2021-04-04 04:23:05 +0300

    A top menu invention and the language switcher updated

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,21 +5,36 @@ import VueRouter, {RouteConfig} from 'vue-router';
 
 Vue.use(VueRouter);
 
-const routes: Array<RouteConfig> = [
+type MenuRoute = RouteConfig & {
+  id: string;
+  showInMenu: boolean;
+  icon: string;
+  title: string;
+};
+
+export const routes: Array<MenuRoute> = [
   {
+    id: '1',
+    showInMenu: true,
+    icon: 'el-icon-notebook-2',
+    title: 'Route.brevetList',
     path: '/brevets',
     name: 'brevet-list',
     component: BrevetList,
   },
   {
+    id: '2',
+    showInMenu: false,
+    icon: 'el-icon-map-location',
+    title: 'Route.brevetInfo',
     path: '/brevet/:uid',
     name: 'brevet-info',
     component: BrevetInfo,
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/BrevetInfo.vue'),
   },
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  // component: () => import(/* webpackChunkName: "about" */ '../views/BrevetInfo.vue'),
 ];
 
 const router = new VueRouter({
