271f35665c89c44d09da1011bd16c9827553b72f
Grigorii Batalov <grisxa@gmail.com>
2021-04-05 03:49:30 +0300

    Put the state into a persistent localStorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,18 @@ import getters from '@/store/getters';
 import mutations from '@/store/mutations';
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
+
+const STORAGE_KEY = 'brevet_online_storage';
+
+const rehydrate = (key: string, storage: unknown) => JSON
+  .parse((storage as Storage).getItem(key) || 'null',
+    // string to Date conversion
+    (name, value) => (['startDate', 'endDate'].includes(name)
+      ? new Date(value)
+      : value));
 
 export default new Vuex.Store<State>({
   strict: true,
@@ -16,6 +26,10 @@ export default new Vuex.Store<State>({
     loading: false,
     locale: process.env.VUE_APP_I18N_LOCALE,
   },
+  plugins: [createPersistedState({
+    key: STORAGE_KEY,
+    getState: rehydrate,
+  })],
   getters,
   mutations,
   actions,
